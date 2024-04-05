ad748d7652bc3a4076f544c7e761db6392b11ed7
FavourPreciousOchonogor(Precy2020) <f.ochonogor@native.semicolon.africa>
2024-04-05 12:33:56 +0100

    first commit

---
+++ src/test/java/com/example/dockerhub/DockerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
