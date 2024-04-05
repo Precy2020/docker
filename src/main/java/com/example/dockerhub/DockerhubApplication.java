7b28cd56c1d3c142d6de71f10fdd855b929e52dc
FavourPreciousOchonogor(Precy2020) <f.ochonogor@native.semicolon.africa>
2024-04-05 13:00:57 +0100

    add

--- src/main/java/com/example/dockerhub/DockerhubApplication.java
+++ src/main/java/com/example/dockerhub/DockerhubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerhubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerhubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DockerhubApplication.class, args);
+    }
 
 }
